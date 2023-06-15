c297b7fecd1e9c3852e5f5a5daf8031bf52ce5a1
Bulat Valiakhmetov <61677060+vbucode@users.noreply.github.com>
2023-06-15 17:28:00 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 print("\x1b[2J", end = "")
 board = [[" ", "a", "b", "c", "d", "e", "f", "g", "h", " "],
-        ["8", "R", "N", "B", "Q", "K", "B", "N", "R", "8"],
-        ["7", "p", "p", "p", "p", "p", "p", "p", "p", "7"],
+        ["8", "R1", "N1", "B1", "Q1", "K1", "B1", "N1", "R1", "8"],
+        ["7", "p1", "p1", "p1", "p1", "p1", "p1", "p1", "p1", "7"],
         ["6", "x", "x", "x", "x", "x", "x", "x", "x", "6"],
         ["5", "x", "x", "x", "x", "x", "x", "x", "x", "5"],
         ["4", "x", "x", "x", "x", "x", "x", "x", "x", "4"],
         ["3", "x", "x", "x", "x", "x", "x", "x", "x", "3"],
-        ["2", "p", "p", "p", "p", "p", "p", "p", "p", "2"],
-        ["1", "R", "N", "B", "Q", "K", "B", "N", "R", "1"],
+        ["2", "p2", "p2", "p2", "p2", "p2", "p2", "p2", "p2", "2"],
+        ["1", "R2", "N2", "B2", "Q2", "K2", "B2", "N2", "R2", "1"],
         [" ", "a", "b", "c", "d", "e", "f", "g", "h", " "]]
 while True:
     print('\x1b[H', end='')
@@ -20,8 +20,9 @@ while True:
     m = ""
     for x, i in enumerate(board[0]):
         if choose[0] == i:
-            m = board[int(choose[1])][x]
-            board[int(choose[1])][x] = "x" 
+            if board[int(choose[1])][x].endswith("1"):
+                m = board[int(choose[1])][x]
+                board[int(choose[1])][x] = "x" 
     for x, i in enumerate(board[0]):
         if step[0] == i:
             board[int(step[1])][x] = m
