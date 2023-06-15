2125300079ac9344c69275c1b87e9c16e7f061cd
Bulat Valiakhmetov <61677060+vbucode@users.noreply.github.com>
2023-06-15 15:54:49 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
-
 print("\x1b[2J", end = "")
+board = [[" ", "a", "b", "c", "d", "e", "f", "g", "h", " "],
+        ["8", "R", "N", "B", "Q", "K", "B", "N", "R", "8"],
+        ["7", "p", "p", "p", "p", "p", "p", "p", "p", "7"],
+        ["6", "x", "x", "x", "x", "x", "x", "x", "x", "6"],
+        ["5", "x", "x", "x", "x", "x", "x", "x", "x", "5"],
+        ["4", "x", "x", "x", "x", "x", "x", "x", "x", "4"],
+        ["3", "x", "x", "x", "x", "x", "x", "x", "x", "3"],
+        ["2", "p", "p", "p", "p", "p", "p", "p", "p", "2"],
+        ["1", "R", "N", "B", "Q", "K", "B", "N", "R", "1"],
+        [" ", "a", "b", "c", "d", "e", "f", "g", "h", " "]]
 while True:
     print('\x1b[H', end='')
-    print("\n")
-    print("\nchess with computer\n")
-    board = [[" ", "a", "b", "c", "d", "e", "f", "g", "h", " "],
-         ["8", "R", "N", "B", "Q", "K", "B", "N", "R", "8"],
-         ["7", "p", "p", "p", "p", "p", "p", "p", "p", "7"],
-         ["6", "x", "x", "x", "x", "x", "x", "x", "x", "6"],
-         ["5", "x", "x", "x", "x", "x", "x", "x", "x", "5"],
-         ["4", "x", "x", "x", "x", "x", "x", "x", "x", "4"],
-         ["3", "x", "x", "x", "x", "x", "x", "x", "x", "3"],
-         ["2", "p", "p", "p", "p", "p", "p", "p", "p", "2"],
-         ["1", "R", "N", "B", "Q", "K", "B", "N", "R", "1"],
-         [" ", "a", "b", "c", "d", "e", "f", "g", "h", " "]]
+    print("\nChess\n")
     for i in board:
         print(" ".join(i))
     print("\n")
-    step = input("It's your move. Type row and column like e3. ")
-    for i in board[0]:
-        if step[0] in i:
-            pass
-                
+    choose = input("It's your move. Type like e2. ")
+    step = input("Move to. Type like e3. ")
+    m = ""
+    for x, i in enumerate(board[0]):
+        if choose[0] == i:
+            m = board[int(choose[1])][x]
+            board[int(choose[1])][x] = "x" 
+    for x, i in enumerate(board[0]):
+        if step[0] == i:
+            board[int(step[1])][x] = m
     print('\x1b[H', end='')
         
